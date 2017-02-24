7278e4d3b90fc0913131e5e7aa5881824ecb41ab
aditi74 <aditisharma1627@gmail.com>
2017-02-25 01:50:30 +0530

    Important Questions

--- src/DynamicProgramming/LongestPalindromSubstring.java
+++ src/DynamicProgramming/LongestPalindromSubstring.java
@@ -6,12 +6,12 @@ package DynamicProgramming;
 public class LongestPalindromSubstring {
     public static void main(String[] args) {
 
-        String str= "cbbd";
+        String str= "aba";
 
         char[] arr= str.toCharArray();
         LongestPalindromSubstring longestPalindromSubstring= new LongestPalindromSubstring();
-        System.out.println(longestPalindromSubstring.recursiveLPSubstring(arr,0,arr.length-1));
-
+       // System.out.println(longestPalindromSubstring.recursiveLPSubstring(arr,0,arr.length-1));
+      // System.out.println(longestPalindromSubstring.DynamicLPSubstring(arr,str));
     }
 
 public int recursiveLPSubstring(char[] arr,int i,int j)
@@ -21,7 +21,7 @@ public int recursiveLPSubstring(char[] arr,int i,int j)
     if (i+1== j && arr[i]==arr[j])
         return 2;
 
-    if (arr[i]==arr[j] && recursiveLPSubstring(arr,i+1,j-1)!=0)
+    if (arr[i]==arr[j] && recursiveLPSubstring(arr,i+1,j-1)==0)
 
     {
       return 2+ recursiveLPSubstring(arr,i+1,j-1);
@@ -33,5 +33,58 @@ public int recursiveLPSubstring(char[] arr,int i,int j)
 
 
 }
+
+
+
+    public String longestPalindrome(String s) {
+        if(s==null || s.length()<=1)
+            return s;
+
+        int len = s.length();
+        int maxLen = 1;
+        boolean [][] dp = new boolean[len][len];
+
+        String longest = null;
+        for(int l=0; l<s.length(); l++){
+            for(int i=0; i<len-l; i++){
+                int j = i+l;
+                if(s.charAt(i)==s.charAt(j) && (j-i<=2||dp[i+1][j-1])){
+                    dp[i][j]=true;
+
+                    if(j-i+1>maxLen){
+                        maxLen = j-i+1;
+                        longest = s.substring(i, j+1);
+                    }
+                }
+            }
+        }
+
+        return longest;
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 }
