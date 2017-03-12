956c56661d567700d3296be09e928508f93a3637
aditi74 <aditisharma1627@gmail.com>
2017-03-13 01:29:26 +0530

    Important Questions

--- src/DynamicProgramming/WordBreakProblem.java
+++ src/DynamicProgramming/WordBreakProblem.java
@@ -17,14 +17,64 @@ public class WordBreakProblem {
 
 
 
-    HashSet<String> set = new HashSet<>();
+    public  static HashSet<String> set = new HashSet<>();
+
 
     public static void main(String[] args) {
        WordBreakProblem wordBreakProblem= new WordBreakProblem();
         List<String> list= new ArrayList<>();
         list.add("leet");
         list.add("code");
-        System.out.println(wordBreakProblem.wordBreak("leetcode",list));
+       // System.out.println(wordBreakProblem.wordBreak("leetcode",list));
+
+       set.add("leet");
+        set.add("code");
+        System.out.println(wordBreakProblem.wordBreakRecursive("leetcode"));
+    }
+
+
+    public boolean    wordBreaKDP(String s)
+    {
+        boolean[][] matrix= new boolean[s.length()][s.length()];
+
+        for (int i=0;i<s.length();i++)
+        {
+            if (set.contains(String.valueOf(s.charAt(i))))
+                matrix[i][i]= true;
+            else
+                matrix[i][i]=false;
+        }
+
+
+        for (int k=2;k<=s.length();k++)
+          for (int i=0;i<s.length()-k;i++)
+          {
+              int j=i+k-1;
+              //if string between i to j is in dictionary T[i][j] is true
+              if (set.contains(s.substring(i,j+1)))
+                  matrix[i][j]=true;
+
+              else {
+                  //find a k between i+1 to j such that T[i][k-1] && T[k][j] are both true
+
+                  for (int m = i + 1; m <= j; m++) {
+                      if (matrix[i][m - 1] == true && matrix[m][j] == true) {
+                          matrix[i][j] = true;
+                          break;
+                      }
+
+
+                  }
+
+
+              }
+          }
+          if (matrix[0][s.length()-1]==true)
+              return true;
+        else
+            return false;
+
+
 
 
 
@@ -75,8 +125,29 @@ public class WordBreakProblem {
 
     }
 
+
+    // recursive solution // we consider each prefix and search it in dictionary. ifthe prefix is present
+    // we recur for the rest of the string
+
     /*public boolean usingDPWordCheck(String string)
     {
 
     }*/
+
+
+    public boolean wordBreakRecursive(String str) {
+        if (str.length() == 0)
+            return true;
+
+
+
+        for (int i = 0; i <str.length(); i++){
+            if (set.contains(str.substring(0, i+1)) && wordBreakRecursive(str.substring(i+1, str.length())))
+                return true;
+    }
+    return false;
+
+
+
+    }
 }
